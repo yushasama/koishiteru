bd491590993e8e32ade2d94cdb5b40eefa646f0c
noromon99 <applemusicgherbo@gmail.com>
2023-10-30 22:39:21 -0700

    xd

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -1,21 +1,23 @@
-import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
-const apiKey = process.env.NEXT_PUBLIC_API_KEY
-const userName = process.env.NEXT_PUBLIC_USER_NAME
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const userName = process.env.NEXT_PUBLIC_USER_NAME;
 
-export async function GET(){
+export async function GET(req:NextApiRequest, res:NextApiResponse) {
   try {
     const response = await axios.get(
       `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${userName}&api_key=${apiKey}&limit=1&nowplaying=true&format=json`
     );
 
     const mostRecentSong = {
-      'title': response.data.recenttracks.track[0].name,
-      'artist': response.data.recenttracks.track[0].artist['#text']
-    }
+      title: response.data.recenttracks.track[0].name,
+      artist: response.data.recenttracks.track[0].artist['#text'],
+    };
 
-    return Response.json(mostRecentSong)
+    return Response.json(mostRecentSong); // Use res.json() to send the response
   } catch (error) {
-    console.error(error)
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' }); // Handle errors and send an error response
   }
 }
