c4baca3341f2dbc086518cc96b54387432274e88
noromon99 <applemusicgherbo@gmail.com>
2023-10-30 21:33:38 -0700

    Spotify API fix
    Fixed error where Spotify API parameters were mismatched

--- app/api/spotify/route.ts
+++ app/api/spotify/route.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export async function GET(){
   try {
     const response = await axios.get(
-      `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${process.env.NEXT_PUBLIC_API_KEY}&api_key=${process.env.NEXT_PUBLIC_USER_NAME}&limit=1&nowplaying=true&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${process.env.NEXT_PUBLIC_USER_NAME}&api_key=${process.env.NEXT_PUBLIC_API_KEY}&limit=1&nowplaying=true&format=json`
     );
 
     const mostRecentSong = {
