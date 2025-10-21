3d023c3078370dd0ec8351e481207a58a76138d2
yushasama <yushasama@users.noreply.github.com>
2025-10-21 11:12:15 -0700

    Tweaked life page scrolling

--- data/lifeSections.ts
+++ data/lifeSections.ts
@@ -7,39 +7,33 @@ export interface LifeSection {
 
 export const lifeSections: LifeSection[] = [
   {
-    header: "Making Dubai Chocolate",
-    subtitle: "02/13/2025",
-    mainText: "I made dubai chocolate with someone I like. It was my first time making chocolate so I'm glad it was with her. We both liked it because it was yummy and fun.",
-    imageSource: "/life/entry_images/1.jpg"
-  },
-  {
     header: "LA → NYC",
     subtitle: "04/02/2025",
     mainText: "I took a plane to NYC for a school field trip. It was my first time traveling alone. It was a little bit scary.",
-    imageSource: "/life/entry_images/2.jpg"
+    imageSource: "/life/entry_images/la_nyc.jpg"
   },
   {
-    header: "St Patrick's Church", 
+    header: "St Patrick's Cathedral", 
     subtitle: "05/03/2025",
-      mainText: "I went to St Patrick's Church. It was big and beautiful. I never went to a church like that before.",
-      imageSource: "/life/entry_images/3.jpg"
+      mainText: "I went to St Patrick's Cathedral. It was big and beautiful. I never went to a church like that before.",
+      imageSource: "/life/entry_images/st_patrick.jpg"
   },
   {
     header: "Thai Curry Pizza", 
     subtitle: "08/28/2025",
     mainText: "My university friend told me to try Thai Curry Pizza. I got the tom yum chicken pizza. It was the best pizza I ever had.",
-    imageSource: "/life/entry_images/4.jpg"
+    imageSource: "/life/entry_images/thai_curry_pizza.jpg"
   },
   {
     header: "Hanging Out With My Childhood Friend", 
     subtitle: "09/14/2025",
-    mainText: "Today I hung out with my childhood friend who is funny. It was the first time we hung out in 5 years. We went to a cafe and ate fried chicken at Koko's. It was fun.",
-    imageSource: "/life/entry_images/5.jpg"
+    mainText: "Today I hung out with my childhood friend who is funny. It was the first time in 5 years. We went to a cafe and then ate fried chicken at Koko's. It was fun.",
+    imageSource: "/life/entry_images/hanging_out_with_childhood_friend.jpg"
   },
   {
     header: "Balboa Beach", 
     subtitle: "10/10/2025",
-    mainText: "I went to Balboa Beach for a bonfire. It was a beautiful day and I got to eat smores.",
-    imageSource: "/life/entry_images/6.jpg"
+    mainText: "I went to Balboa Beach for a bonfire. It was a beautiful day. We got to eat smores and grilled chicken.",
+    imageSource: "/life/entry_images/balboa_beach.jpg"
   }
 ];
